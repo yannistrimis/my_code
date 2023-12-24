7f898d89a2e7c2b40233adb86d63b1a02e8b0feb
yannistrimis <trimisio@msu.edu>
2023-12-24 09:38:06 -0500

    laptop

--- corrfitter/src/envelope_fitter_v1.0.sh
+++ corrfitter/src/envelope_fitter_v1.0.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-vol=16128
-beta=7225
-xg=36836
+vol=1632
+beta=6850
+xg=100
 src="eowfw"
 prefix="nlpi"
-taste="PION_0"
-to_print_state="Eo"
+taste="PION_i5"
+to_print_state="En"
 to_print_nr=0
 
 #fitdir="/home/trimis/spec_data" # CMSE
 #dir="/home/trimis/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # CMSE
 fitdir="/home/yannis/Physics/LQCD/spec_data" # LAPTOP
 dir="/home/yannis/Physics/LQCD/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # LAPTOP
-tdata=65
-tp=128
-n_states=0
-m_states=1
-so="-1.0"
+tdata=17
+tp=32
+n_states=1
+m_states=0
+so="1.0"
 binsize=1
 
 #yesno="prior"
@@ -27,14 +27,14 @@ if [ $1 == "scan" ]
 then
 
 tmin_min=0
-tmin_max=20
+tmin_max=13
 
-tmax_min=25
-tmax_max=25
+tmax_min=17
+tmax_max=17
 
-xq_arr=( "3750" )
+xq_arr=( "100" )
 mom_arr=( "p000" )
-mass_arr=( "0.01448" )
+mass_arr=( "0.01576" )
 
 for xq in ${xq_arr[@]};do
 echo "xq = ${xq}"
@@ -77,12 +77,12 @@ done # xq
 elif [ $1 == "one"  ]
 then
 
-xq="3750"
+xq="100"
 mom="p000"
-tmin=18
-tmax=25
+tmin=5
+tmax=17
 
-mass=0.01448
+mass=0.01576
 
 python3 fitter_v1.0.py <<EOF
 ${dir}
