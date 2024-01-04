993837d7c9f9e35c360d82eea12e81cd1199c4e2
yannistrimis <trimisio@msu.edu>
2024-01-04 10:08:08 -0500

    laptop

--- corrfitter/src/envelope_fitter_v1.0.sh
+++ corrfitter/src/envelope_fitter_v1.0.sh
@@ -5,14 +5,14 @@ beta=7225
 xg=36836
 src="eowfw"
 prefix="nlpi"
-taste="PION_ij"
-to_print_state="o"
+taste="PION_i"
+to_print_state="n"
 to_print_nr=0
 
 #fitdir="/home/trimis/spec_data" # CMSE
 #dir="/home/trimis/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # CMSE
 fitdir="/home/yannis/Physics/LQCD/spec_data" # LAPTOP
-dir="/home/yannis/Physics/LQCD/fnal/all/spec_data/l${vol}b${beta}x${xg}a" # LAPTOP
+dir="/home/yannis/Physics/LQCD/corrs_from_fnal/l${vol}b${beta}x${xg}a" # LAPTOP
 tdata=65
 tp=128
 n_states=1
@@ -26,11 +26,13 @@ yesno="free"
 if [ $1 == "scan" ]
 then
 
-tmin_min=15
-tmin_max=39
+tmin_min=10
+tmin_max=42
+tmin_step=1
 
-tmax_min=43
-tmax_max=43
+tmax_min=55
+tmax_max=55
+tmax_step=5
 
 xq_arr=( "3750" )
 mom_arr=( "p000" )
@@ -48,8 +50,8 @@ then
 rm ${fitdir}/${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.bin${binsize}.E${to_print_state}${to_print_nr}.${yesno}.scanfit
 fi
 
-for ((tmin=${tmin_min};tmin<=${tmin_max};tmin++));do
-for ((tmax=${tmax_min};tmax<=${tmax_max};tmax++));do
+for ((tmin=${tmin_min};tmin<=${tmin_max};tmin=${tmin}+${tmin_step}));do
+for ((tmax=${tmax_min};tmax<=${tmax_max};tmax=${tmax}+${tmax_step}));do
 
 python3 fitter_v1.0.py <<EOF >> ${fitdir}/${prefix}${mom}${src}${vol}b${beta}x${xg}xq${xq}_m${mass}m${mass}${taste}.${n_states}p${m_states}.bin${binsize}.E${to_print_state}${to_print_nr}.${yesno}.scanfit
 ${dir}
@@ -79,8 +81,8 @@ then
 
 xq="3750"
 mom="p000"
-tmin=20
-tmax=43
+tmin=30
+tmax=55
 
 mass=0.01448
 
