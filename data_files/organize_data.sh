ee2f4d06e84d08a53a7de08cbaa1c75a79207a83
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-02-26 15:03:27 -0600

    fnal

---
+++ data_files/organize_data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+awk '$1 == "w" && $2 == "clover" { print $5,$7 }' tuning_schemes.data  > swc.data
+awk '$1 == "w" && $2 == "wilson" { print $5,$7 }' tuning_schemes.data  > sww.data
+awk '$1 == "w" && $2 == "symanzik" { print $5,$7 }' tuning_schemes.data  > sws.data
+awk '$1 == "s" && $2 == "clover" { print $5,$7 }' tuning_schemes.data  > ssc.data
+awk '$1 == "s" && $2 == "wilson" { print $5,$7 }' tuning_schemes.data  > ssw.data
+awk '$1 == "s" && $2 == "symanzik" { print $5,$7 }' tuning_schemes.data  > sss.data
+awk '$1 == "z" && $2 == "clover" { print $5,$7 }' tuning_schemes.data  > szc.data
+awk '$1 == "z" && $2 == "i_clover" { print $5,$7 }' tuning_schemes.data  > szic.data
+awk '$1 == "z" && $2 == "wilson" { print $5,$7 }' tuning_schemes.data  > szw.data
+awk '$1 == "z" && $2 == "symanzik" { print $5,$7 }' tuning_schemes.data  > szs.data
+
+cat <<EOF > betafile.data
+6.9
+7.0
+7.1
+7.2
+7.3
+EOF
+
+paste betafile.data szic.data > beta_szic.data
