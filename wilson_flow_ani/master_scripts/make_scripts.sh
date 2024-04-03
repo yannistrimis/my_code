2b8fe2d3559d7454ed387e14d8ff83e7c66be721
yannistrimis <trimisio@msu.edu>
2024-04-03 16:08:10 -0400

    icer

--- wilson_flow_ani/master_scripts/make_scripts.sh
+++ wilson_flow_ani/master_scripts/make_scripts.sh
@@ -4,32 +4,32 @@
 # IF MULTIPLE DIRECTORIES ARE NEEDED, THE USER CAN CREATE ARRAYS
 # FOR THE CHANGING PARAMETERS.
 
-cluster="fnal"
-n_of_ens=10
+cluster="icer"
+n_of_ens=4
 
-nx=16
-nt=64
+nx=20
+nt=160
 
-beta_name="7200"
-xi_0_name_arr=("3000" "3100" "3200" "3300" "3400" "3500" "3600" "3700" "3800" "3900")
+beta_name="7100"
+xi_0_name_arr=("6800" "6900" "7000" "7100")
 stream="a"
 
-xi_f=4.00
-xi_f_name="400"
+xi_f=8.00
+xi_f_name="800"
 
 flow_action="wilson"
 exp_order="16"
 dt="0.015625"
 stoptime="3.0"
 
-sbatch_time="6:00:00"
+sbatch_time="10:00:00"
 sbatch_nodes=4 # N/A WHEN icer IS SELECTED
 sbatch_ntasks=128
-sbatch_jobname_arr=("w3000" "w3100" "w3200" "w3300" "w3400" "w3500" "w3600" "w3700" "w3800" "w3900")
+sbatch_jobname_arr=("w6800" "w6900" "w7000" "w7100")
 
 n_of_sub=4
-n_of_lat=100
-first_lattice=101
+n_of_lat=25
+first_lattice=51
 
 for (( i_ens=0; i_ens<${n_of_ens}; i_ens++ )); do
 
