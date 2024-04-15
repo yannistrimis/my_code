004991003540166fcf321dd0b15de1686e06b1f3
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-04-15 15:27:22 -0500

    fnal

--- pure_gauge_ani_tuning/scripts/temp.envelope.sh
+++ pure_gauge_ani_tuning/scripts/temp.envelope.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 flow_type_arr=("w")
-obs_type_arr=("clover")
+obs_type_arr=("clover" "i_clover" "wilson" "symanzik")
 
 for flow_type in ${flow_type_arr[@]}; do
 for obs_type in ${obs_type_arr[@]}; do
--- wilson_flow_ani/master_scripts/make_scripts.sh
+++ wilson_flow_ani/master_scripts/make_scripts.sh
@@ -10,22 +10,22 @@ n_of_ens=10
 nx=16
 nt=64
 
-beta_name="7300"
+beta_name="7000"
 xi_0_name_arr=("3000" "3100" "3200" "3300" "3400" "3500" "3600" "3700" "3800" "3900")
 stream="a"
 
 xi_f=4.00
 xi_f_name="400"
 
-flow_action="wilson"
+flow_action="symanzik"
 exp_order="16"
 dt="0.015625"
 stoptime="3.0"
 
-sbatch_time="6:00:00"
+sbatch_time="10:00:00"
 sbatch_nodes=4 # N/A WHEN icer IS SELECTED
 sbatch_ntasks=128
-sbatch_jobname_arr=("w3000" "w3100" "w3200" "w3300" "w3400" "w3500" "w3600" "w3700" "w3800" "w3900")
+sbatch_jobname_arr=("s3000" "s3100" "s3200" "s3300" "s3400" "s3500" "s3600" "s3700" "s3800" "s3900")
 
 n_of_sub=4
 n_of_lat=100
