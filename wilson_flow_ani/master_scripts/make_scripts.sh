3a8262056bfadf5a4152a15daa089bb337094ca9
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-04-17 11:39:52 -0500

    fnal

--- wilson_flow_ani/master_scripts/make_scripts.sh
+++ wilson_flow_ani/master_scripts/make_scripts.sh
@@ -10,14 +10,14 @@ n_of_ens=10
 nx=16
 nt=64
 
-beta_name="7200"
+beta_name="7000"
 xi_0_name_arr=("3000" "3100" "3200" "3300" "3400" "3500" "3600" "3700" "3800" "3900")
 stream="a"
 
 xi_f=4.00
 xi_f_name="400"
 
-flow_action="symanzik"
+flow_action="zeuthen"
 exp_order="16"
 dt="0.015625"
 stoptime="3.0"
@@ -25,9 +25,9 @@ stoptime="3.0"
 sbatch_time="10:00:00"
 sbatch_nodes=4 # N/A WHEN icer IS SELECTED
 sbatch_ntasks=128
-sbatch_jobname_arr=("s3000" "s3100" "s3200" "s3300" "s3400" "s3500" "s3600" "s3700" "s3800" "s3900")
+sbatch_jobname_arr=("z3000" "z3100" "z3200" "z3300" "z3400" "z3500" "z3600" "z3700" "z3800" "z3900")
 
-n_of_sub=4
+n_of_sub=3
 n_of_lat=100
 first_lattice=101
 
