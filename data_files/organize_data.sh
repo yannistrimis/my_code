1d5aebd9fb730362665b13144ee9b837005fb9a5
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-04-18 12:38:41 -0500

    fnal

--- data_files/organize_data.sh
+++ data_files/organize_data.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-awk '$1 == "w" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > swc_xiR_4.data
-awk '$1 == "w" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > swi_xiR_4.data
-awk '$1 == "w" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > sww_xiR_4.data
-awk '$1 == "w" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > sws_xiR_4.data
-awk '$1 == "s" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > ssc_xiR_4.data
-awk '$1 == "s" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > ssi_xiR_4.data
-awk '$1 == "s" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > ssw_xiR_4.data
-awk '$1 == "s" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > sss_xiR_4.data
-# awk '$1 == "z" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > szc_xiR_4.data
-# awk '$1 == "z" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > szi_xiR_4.data
-# awk '$1 == "z" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > szw_xiR_4.data
-# awk '$1 == "z" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_4.data  > szs_xiR_4.data
+awk '$1 == "w" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > swc_xiR_2.data
+awk '$1 == "w" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > swi_xiR_2.data
+awk '$1 == "w" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > sww_xiR_2.data
+awk '$1 == "w" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > sws_xiR_2.data
+awk '$1 == "s" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > ssc_xiR_2.data
+awk '$1 == "s" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > ssi_xiR_2.data
+awk '$1 == "s" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > ssw_xiR_2.data
+awk '$1 == "s" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > sss_xiR_2.data
+awk '$1 == "z" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szc_xiR_2.data
+awk '$1 == "z" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szi_xiR_2.data
+awk '$1 == "z" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szw_xiR_2.data
+awk '$1 == "z" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szs_xiR_2.data
 
 cat <<EOF > betafile.data
 6.9
@@ -21,7 +21,7 @@ cat <<EOF > betafile.data
 7.3
 EOF
 
-for file in ???_xiR_4.data
+for file in ???_xiR_2.data
 do
 
 paste betafile.data ${file} > beta_${file}
