c73afb1d873a2da53d728762e32694e973f637bc
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-04-25 15:20:16 -0500

    fnal

--- data_files/organize_data.sh
+++ data_files/organize_data.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-xiR="4"
+xiR="2"
 
-awk '$1 == "w" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > swc_xiR_${xiR}.data
-awk '$1 == "w" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > swi_xiR_${xiR}.data
-awk '$1 == "w" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > sww_xiR_${xiR}.data
-awk '$1 == "w" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > sws_xiR_${xiR}.data
-awk '$1 == "s" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > ssc_xiR_${xiR}.data
-awk '$1 == "s" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > ssi_xiR_${xiR}.data
-awk '$1 == "s" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > ssw_xiR_${xiR}.data
-awk '$1 == "s" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > sss_xiR_${xiR}.data
-awk '$1 == "z" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szc_xiR_${xiR}.data
-awk '$1 == "z" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szi_xiR_${xiR}.data
-awk '$1 == "z" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szw_xiR_${xiR}.data
-awk '$1 == "z" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_2.data  > szs_xiR_${xiR}.data
+awk '$1 == "w" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > swc_xiR_${xiR}.data
+awk '$1 == "w" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > swi_xiR_${xiR}.data
+awk '$1 == "w" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > sww_xiR_${xiR}.data
+awk '$1 == "w" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > sws_xiR_${xiR}.data
+awk '$1 == "s" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > ssc_xiR_${xiR}.data
+awk '$1 == "s" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > ssi_xiR_${xiR}.data
+awk '$1 == "s" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > ssw_xiR_${xiR}.data
+awk '$1 == "s" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > sss_xiR_${xiR}.data
+awk '$1 == "z" && $2 == "clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > szc_xiR_${xiR}.data
+awk '$1 == "z" && $2 == "i_clover" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > szi_xiR_${xiR}.data
+awk '$1 == "z" && $2 == "wilson" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > szw_xiR_${xiR}.data
+awk '$1 == "z" && $2 == "symanzik" { print $5,$7,$10,$12 }' tuning_schemes_xiR_${xiR}.data  > szs_xiR_${xiR}.data
 
 cat <<EOF > betafile.data
 6.9
--- data_files/plot_schemes.plt
+++ data_files/plot_schemes.plt
@@ -1,22 +1,22 @@
 reset
 set terminal postscript dashed color \
  font 'Helvetica,18' linewidth 3
-set output "schemes_xiR_4.ps"
+set output "schemes_xiR_2_norm.ps"
 
 set key top right
 set key font ",12"
 
-plot [6.875:7.325][] "beta_swc_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "red" title "SWC",\
-"beta_sww_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "green" title "SWW",\
-"beta_sws_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "blue" title "SWS",\
-"beta_ssc_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "cyan" title "SSC",\
-"beta_ssw_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "violet" title "SSW",\
-"beta_sss_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "yellow" title "SSS",\
-"beta_szi_xiR_4.data" u 1:2:3 w err pt 1 lc rgb "dark-green" title "SZI",\
-"beta_swc_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "red" notitle,\
-"beta_sww_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "green" notitle,\
-"beta_sws_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "blue" notitle,\
-"beta_ssc_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "cyan" notitle,\
-"beta_ssw_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "violet" notitle,\
-"beta_sss_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "yellow" notitle,\
-"beta_szi_xiR_4.data" u 1:2:3 w l lw 1 lc rgb "dark-green" notitle
+plot [6.875:7.325][] "beta_swc_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "red" title "SWC",\
+"beta_sww_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "green" title "SWW",\
+"beta_sws_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "blue" title "SWS",\
+"beta_ssc_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "cyan" title "SSC",\
+"beta_ssw_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "violet" title "SSW",\
+"beta_sss_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "yellow" title "SSS",\
+"beta_szi_xiR_2.data" u 1:($2/2):($3/2) w err pt 1 lc rgb "dark-green" title "SZI",\
+"beta_swc_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "red" notitle,\
+"beta_sww_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "green" notitle,\
+"beta_sws_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "blue" notitle,\
+"beta_ssc_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "cyan" notitle,\
+"beta_ssw_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "violet" notitle,\
+"beta_sss_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "yellow" notitle,\
+"beta_szi_xiR_2.data" u 1:($2/2):($3/2) w l lw 1 lc rgb "dark-green" notitle
