53442ac57340ad415c3974e8d0cbc4d0177bf267
yannistrimis <trimisio@msu.edu>
2024-05-06 17:04:57 -0400

    iCER

--- wilson_flow_ani/master_scripts/make_scripts.sh
+++ wilson_flow_ani/master_scripts/make_scripts.sh
@@ -5,13 +5,13 @@
 # FOR THE CHANGING PARAMETERS.
 
 cluster="icer"
-n_of_ens=1
+n_of_ens=5
 
 nx=20
 nt=160
 
-beta_name="7100"
-xi_0_name_arr=("6700")
+beta_name="7200"
+xi_0_name_arr=("6700" "6800" "6900" "7000" "7100")
 stream="a"
 
 xi_f=8.00
@@ -24,8 +24,8 @@ stoptime="3.0"
 
 sbatch_time="10:00:00"
 sbatch_nodes=4 # N/A WHEN icer IS SELECTED
-sbatch_ntasks=128
-sbatch_jobname_arr=("w6700")
+sbatch_ntasks=100
+sbatch_jobname_arr=("w6700" "w6800" "w6900" "w7000" "w7100")
 
 n_of_sub=5
 n_of_lat=20
