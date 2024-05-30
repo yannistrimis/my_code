5d47add2e135005da9477efcc451f18d67c98046
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-05-30 12:18:00 -0500

    fnal

--- pure_gauge_spectrum/master_scripts/make_scripts.sh
+++ pure_gauge_spectrum/master_scripts/make_scripts.sh
@@ -4,25 +4,25 @@
 # IF MULTIPLE DIRECTORIES ARE NEEDED, THE USER CAN CREATE ARRAYS
 # FOR THE CHANGING PARAMETERS.
 
-cluster="icer"
+cluster="fnal"
 n_of_ens=1
 
 nx=16
-nt=64
+nt=32
 
 set_i_lat=101
 set_seed=78324
 
-beta_name="704115"
-xi_0_name="181411"
+beta_name="681823"
+xi_0_name="100000"
 stream="a"
 
 u0=1
 
 set_source_start=0
 n_sources=2
-source_inc=32 # CHANGE ACCORDING TO nt
-source_prec=18 # CHANGE ACCORDING TO nt
+source_inc=16 # CHANGE ACCORDING TO nt
+source_prec=10 # CHANGE ACCORDING TO nt
 
 nmasses=5
 mass1=0.03
@@ -32,9 +32,9 @@ mass4=0.09
 mass5=0.11
 
 nxq=1
-xq1=1.98
+xq1=1.00
 
-xq1_name="1980"
+xq1_name="1000"
 
 err=1e-6
 max_cg_iterations=300
@@ -44,7 +44,7 @@ precision=2
 sbatch_time="04:00:00"
 sbatch_nodes=4 # N/A WHEN icer IS SELECTED
 sbatch_ntasks=128
-sbatch_jobname="str_xi2"
+sbatch_jobname="str_xi1"
 
 prefix="str"
 build_prefix="str"
