645c730dc8cebd68a259fadbaad02bc0f12f89ec
yannistrimis <trimisio@msu.edu>
2024-06-06 13:27:46 -0400

    nlpi

--- pure_gauge_spectrum/master_scripts/make_scripts.sh
+++ pure_gauge_spectrum/master_scripts/make_scripts.sh
@@ -4,33 +4,33 @@
 # IF MULTIPLE DIRECTORIES ARE NEEDED, THE USER CAN CREATE ARRAYS
 # FOR THE CHANGING PARAMETERS.
 
-cluster="fnal"
+cluster="icer"
 n_of_ens=1
 
 nx=16
-nt=32
+nt=64
 
 set_i_lat=101
 set_seed=78324
 
-beta_name="681823"
-xi_0_name="100000"
+beta_name="704115"
+xi_0_name="181411"
 stream="a"
 
 u0=1
 
 set_source_start=0
 n_sources=2
-source_inc=16 # CHANGE ACCORDING TO nt
-source_prec=10 # CHANGE ACCORDING TO nt
+source_inc=32 # CHANGE ACCORDING TO nt
+source_prec=18 # CHANGE ACCORDING TO nt
 
 nmasses=1
-mass1=0.01524
+mass1=0.0146
 
 nxq=1
-xq1=1.000
+xq1=1.980
 
-xq1_name="1000"
+xq1_name="1980"
 
 err=1e-6
 max_cg_iterations=300
@@ -40,7 +40,7 @@ precision=2
 sbatch_time="16:00:00"
 sbatch_nodes=4 # N/A WHEN icer IS SELECTED
 sbatch_ntasks=128
-sbatch_jobname="nlpi_xi1"
+sbatch_jobname="nlpi_xi2"
 
 prefix="nlpi"
 build_prefix="nlpi"
