afc22edf2517b4cd30adb9e6deb707a9794b9e9d
Ioannis Trimis <trimisio@lq.fnal.gov>
2024-06-21 08:13:09 -0500

    fnal

--- pure_gauge_ani_generation/fnal_scripts_1664b704115x181411z/params.sh
+++ pure_gauge_ani_generation/fnal_scripts_1664b704115x181411z/params.sh
@@ -9,8 +9,8 @@ n_of_sub=1
 
 nx=16
 ny=16
-nz=16
-nt=64
+nz=64
+nt=16
 
 # MILC convention in the improved action is: beta=10/g^2
 # Here we use plaquette action and so that is not relevant.
@@ -21,11 +21,11 @@ nt=64
 # beta_s=beta/xi_0
 # beta_t=beta*xi_0
 
-beta_s=3.85184 #in the MILC colde this appears first
-beta_t=12.48830 #and this appears second
+beta_s=3.88132 #in the MILC colde this appears first
+beta_t=12.77342 #and this appears second
 
-beta_name="693563"
-xi_0_name="180060"
+beta_name="704115"
+xi_0_name="181411"
 
 warms=0
 trajecs=20
@@ -34,25 +34,25 @@ steps_per_trajectory=4
 u0=1.0 # THIS IS !=1 FOR 1-LOOP SYMANZIK
 qhb_steps=1
 
-stream="a"
+stream="z"
 
-ensemble="1664b693563x180060a"
-lat_name="l1664b693563x180060a"
-out_name="out1664b693563x180060a"
+ensemble="1664b704115x181411z"
+lat_name="l1664b704115x181411z"
+out_name="out1664b704115x181411z"
 
 
-directory="/lustre1/ahisq/yannis_puregauge/lattices/l1664b693563x180060a"
-out_dir="/project/ahisq/yannis_puregauge/outputs/l1664b693563x180060a"
+directory="/lustre1/ahisq/yannis_puregauge/lattices/l1664b704115x181411z"
+out_dir="/project/ahisq/yannis_puregauge/outputs/l1664b704115x181411z"
 path_build="/home/trimisio/all/my_code/pure_gauge_ani_generation/build"
-run_dir="/project/ahisq/yannis_puregauge/runs/rungenl1664b693563x180060a"
-submit_dir="/project/ahisq/yannis_puregauge/submits/subgenl1664b693563x180060a"
+run_dir="/project/ahisq/yannis_puregauge/runs/rungenl1664b704115x181411z"
+submit_dir="/project/ahisq/yannis_puregauge/submits/subgenl1664b704115x181411z"
 
 executable="su3_ora_symzk0_a_dbl_gcc12openmpi4_20231201"
 
 sbatch_time="20:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
-sbatch_jobname="ga02x2"
+sbatch_jobname="ga02z"
 sbatch_module1="gcc/12"
 sbatch_module2="openmpi/4"
 
--- pure_gauge_ani_generation/master_scripts/make_scripts.sh
+++ pure_gauge_ani_generation/master_scripts/make_scripts.sh
@@ -10,18 +10,18 @@ n_of_ens=1
 nx=16
 nt=64
 
-beta=6.93563
-beta_name="693563"
+beta=7.04115
+beta_name="704115"
 
-xi_0_arr=(1.80060)
-xi_0_name_arr=("180060")
+xi_0_arr=(1.81411)
+xi_0_name_arr=("181411")
 
-stream="a"
+stream="z"
 
 sbatch_time="20:00:00"
 sbatch_nodes=4
 sbatch_ntasks=128
-sbatch_jobname_arr=("ga02x2")
+sbatch_jobname_arr=("ga02z")
 
 n_of_sub=1
 n_of_lat=500
