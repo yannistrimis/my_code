68036e31df86ed780413661aa78c59b62312e30d
Ioannis Trimis <trimisio@lq.fnal.gov>
2025-02-03 13:58:45 -0600

    fnal

--- NRQCD/code_v0.3/control.sh
+++ NRQCD/code_v0.3/control.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 lat=$1
+corr=$2
 
-./mainprogram <<EOF
+./mainprogram_v0.3 <<EOF
 "${lat}"
+"${corr}.corr1s0"
+"${corr}.corr3s1"
 EOF
--- NRQCD/code_v0.3/envelope_mainprogram.sh
+++ NRQCD/code_v0.3/envelope_mainprogram.sh
@@ -6,7 +6,7 @@ out_dir="/project/ahisq/yannis_puregauge/outputs/l2040b708567x181690a"
 M=1900
 
 i_lat=101
-n_lat=400
+n_lat=2
 
 n_meas=0
 
@@ -19,7 +19,7 @@ then
 rm ${out_dir}/NRQCD_M${M}_${lat_name}.${i_lat}
 fi
 
-bash control.sh ${lat_dir}/${lat_name}.trunc.${i_lat} | tee ${out_dir}/NRQCD_M${M}_${lat_name}.${i_lat} >/dev/null
+bash control.sh ${lat_dir}/${lat_name}.trunc.${i_lat} ${out_dir}/NRQCD_M${M}_${lat_name}.${i_lat} | tee ${out_dir}/NRQCD_M${M}_${lat_name}.${i_lat} >/dev/null
 
 text="RUNNING COMPLETED"
 complete_flag=$(bash is_complete.sh ${out_dir}/NRQCD_M${M}_${lat_name}.${i_lat} ${text})
