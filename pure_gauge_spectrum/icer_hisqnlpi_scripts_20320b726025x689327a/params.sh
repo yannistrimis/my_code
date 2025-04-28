325fce5c0e202d9ad57eea213a61107ec19cc8e0
yannistrimis <trimisio@msu.edu>
2025-04-28 16:33:49 -0400

    icer

--- pure_gauge_spectrum/icer_hisqnlpi_scripts_20320b726025x689327a/params.sh
+++ pure_gauge_spectrum/icer_hisqnlpi_scripts_20320b726025x689327a/params.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 cluster=icer
@@ -6,8 +5,8 @@ cluster=icer
 n_of_lat=1000
 n_of_sub=1
 
-set_i_lat=404
-set_seed=78627
+set_i_lat=405
+set_seed=78628
 
 nx=20
 ny=20
@@ -26,7 +25,7 @@ xq_name_arr=( 7870      )
 
 u0=1
 
-set_source_start=46
+set_source_start=128
 n_sources=2
 source_inc=160
 source_prec=70
@@ -36,8 +35,7 @@ err=1e-6
 max_cg_iterations=300
 precision=2
 
-# build_script=build_input_nlpi_new.sh
-build_script=build_input_nlpi.sh
+build_script=build_input_nlpi_new.sh
 
 directory="/mnt/scratch/trimisio/lattices/l20320b726025x689327a"
 out_dir="/mnt/home/trimisio/outputs/pure_gauge_spec/l20320b726025x689327a"
@@ -45,7 +43,7 @@ path_build="/mnt/home/trimisio/my_code/pure_gauge_spectrum/build"
 run_dir="/mnt/scratch/trimisio/runs/runspechisqnlpil20320b726025x689327a"
 submit_dir="/mnt/home/trimisio/submits/subspechisqnlpil20320b726025x689327a"
 
-executable="ks_spectrum_ani_hisq_dbl_GCC12OpenMPI4_20250417"
+executable="ks_spectrum_ani_hisq_dbl_GCC12OpenMPI4_20250428"
 
 sbatch_time="20:00:00"
 sbatch_nodes="5"
