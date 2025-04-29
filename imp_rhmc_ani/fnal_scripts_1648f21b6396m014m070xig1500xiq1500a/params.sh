7fd2757cd8f6c33405ea02146e512d1d5128e0de
Ioannis Trimis <trimisio@lq.fnal.gov>
2025-04-29 11:37:52 -0500

    fnal

--- imp_rhmc_ani/fnal_scripts_1648f21b6396m014m070xig1500xiq1500a/params.sh
+++ imp_rhmc_ani/fnal_scripts_1648f21b6396m014m070xig1500xiq1500a/params.sh
@@ -4,7 +4,7 @@
 cluster=fnal
 
 init_seed=1158
-n_of_lat=1
+n_of_lat=1000
 n_of_sub=1
 
 nx=16
@@ -21,7 +21,7 @@ dyn_mass_2=0.07
 rationals_file="rationals.m014m070"
 
 warms=0
-trajecs=100
+trajecs=5
 traj_between_meas=1
 microcanonical_time_step=0.04545
 steps_per_trajectory=22
@@ -37,10 +37,10 @@ path_build="/home/trimisio/all/my_code/imp_rhmc_ani/build"
 run_dir="/project/ahisq/yannis_dyn/runs/rungenl1648f21b6396m014m070xig1500xiq1500a"
 submit_dir="/project/ahisq/yannis_dyn/submits/subgenl1648f21b6396m014m070xig1500xiq1500a"
 
-# executable="su3_rhmc_naive_a_dbl_gcc12openmpi4_20250421"
-executable="su3_rhmd_naive_a_dbl_gcc12openmpi4_20250410"
+executable="su3_rhmc_naive_a_dbl_gcc12openmpi4_20250421"
+# executable="su3_rhmd_naive_a_dbl_gcc12openmpi4_20250410"
 
-sbatch_time="10:00:00"
+sbatch_time="20:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
 sbatch_jobname="x15dyn"
---
+++ wilson_flow_ani/master_scripts/make_scripts_dyn.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+# THIS SCRIPT CREATES DIRECTORIES FOR PURE GAUGE GRADIENT FLOW.
+# IF MULTIPLE DIRECTORIES ARE NEEDED, THE USER CAN CREATE ARRAYS
+# FOR THE CHANGING PARAMETERS.
+
+cluster="fnal"
+n_of_ens=1
+
+nx=16
+nt=32
+
+beta_name="6396"
+flavors="f21"
+masses="m014m070"
+xig="1000"
+xiq="1000"
+
+
+stream="a"
+
+xi_f=1.00
+xi_f_name="100"
+
+flow_action="wilson"
+exp_order="16"
+dt="0.015625"
+stoptime=("10.0") # CAREFUL!!!
+
+sbatch_time="20:00:00"
+sbatch_nodes=2 # MAY OR MAY NOT BE RELEVANT
+sbatch_ntasks_per_node=NA # MAY OR MAY NOT BE RELEVANT
+sbatch_ntasks=64 # IN HYPER_SL32 EACH SUBLAT SHOULD HAVE MULTIPLE OF 32 POINTS
+sbatch_jobname="10dywfl"
+
+n_of_sub=1
+n_of_lat=200
+first_lattice=201
+
+for (( i_ens=0; i_ens<${n_of_ens}; i_ens++ )); do
+
+# SUBSTITUTE ARRAY ELEMENTS HERE, IF ANY
+
+
+# SUBSTITUTE ARRAY ELEMENTS HERE, IF ANY
+
+ensemble_nostream="${nx}${nt}${flavors}b${beta_name}${masses}xig${xig}xiq${xiq}"
+ensemble="${ensemble_nostream}${stream}"
+lat_name="l${ensemble}"
+
+if [ ${flow_action} == "wilson"  ]
+then
+prefix="wflow"
+elif [ ${flow_action} == "symanzik" ]
+then
+prefix="sflow"
+elif [ ${flow_action} == "zeuthen" ]
+then
+prefix="zflow"
+fi
+
+out_name="${prefix}${ensemble_nostream}xf${xi_f_name}${stream}_dt${dt}"
+my_dir="${cluster}_${prefix}_scripts_${ensemble}"
+
+cd ..
+mkdir ${my_dir}
+cd master_scripts
+
+# ==================================================================================
+# ==================================================================================
+cat <<EOF > ../${my_dir}/params.sh
+
+#!/bin/bash
+
+cluster=${cluster}
+
+first_lattice=${first_lattice}
+
+n_of_lat=${n_of_lat}
+n_of_sub=${n_of_sub}
+
+nx=${nx}
+ny=${nx}
+nz=${nx}
+nt=${nt}
+
+lat_name="${lat_name}"
+out_name="${out_name}"
+
+xi_f=${xi_f}
+
+flow_action="${flow_action}"
+exp_order="${exp_order}"
+dt="${dt}"
+stoptime="${stoptime}"
+
+EOF
+
+if [ ${cluster} == "icer" ]
+then
+
+cat <<EOF >> ../${my_dir}/params.sh
+
+directory="/mnt/scratch/trimisio/lattices/${lat_name}"
+out_dir="/mnt/home/trimisio/outputs/${lat_name}"
+path_build="/mnt/home/trimisio/my_code/wilson_flow_ani/build"
+run_dir="/mnt/scratch/trimisio/runs/run${prefix}${lat_name}"
+submit_dir="/mnt/home/trimisio/submits/sub${prefix}${lat_name}"
+
+executable="wilson_flow_bbb_a_dbl_GCC12OpenMPI4_20250422"
+
+sbatch_time="${sbatch_time}"
+sbatch_nodes="${sbatch_nodes}"
+sbatch_ntasks_per_node="${sbatch_ntasks_per_node}"
+sbatch_ntasks="${sbatch_ntasks}"
+sbatch_jobname="${sbatch_jobname}"
+sbatch_module1="GCC/12"
+sbatch_module2="OpenMPI/4"
+
+EOF
+
+elif [ ${cluster} == "fnal" ]
+then
+
+cat <<EOF >> ../${my_dir}/params.sh
+
+directory="/lustre1/ahisq/yannis_dyn/lattices/${lat_name}"
+out_dir="/project/ahisq/yannis_dyn/outputs/${lat_name}"
+path_build="/home/trimisio/all/my_code/wilson_flow_ani/build"
+run_dir="/project/ahisq/yannis_dyn/runs/run${prefix}${lat_name}"
+submit_dir="/project/ahisq/yannis_dyn/submits/sub${prefix}${lat_name}"
+
+executable="region_flow_bbb_a_dbl_gcc12openmpi4_20240212"
+
+sbatch_time="${sbatch_time}"
+sbatch_nodes="${sbatch_nodes}"
+sbatch_ntasks="${sbatch_ntasks}"
+sbatch_jobname="${sbatch_jobname}"
+sbatch_module1="gcc/12"
+sbatch_module2="openmpi/4"
+
+EOF
+
+elif [ ${cluster} == "nersc" ]
+then
+
+cat <<EOF >> ../${my_dir}/params.sh
+
+directory="/global/cfs/projectdirs/m1416/yannis_puregauge/lattices/${lat_name}"
+out_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/outputs/${lat_name}"
+path_build="/global/homes/t/trimisio/my_code/pure_gauge_ani_generation/build"
+run_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/runs/rungen${lat_name}"
+submit_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/submits/subgen${lat_name}"
+
+executable=""
+
+sbatch_time="${sbatch_time}"
+sbatch_nodes="${sbatch_nodes}"
+sbatch_ntasks="${sbatch_ntasks}"
+sbatch_jobname="${sbatch_jobname}"
+
+EOF
+
+fi
+
+# =====================================================================================
+# =====================================================================================
+
+
+cp control_script.sh ../${my_dir}/control_script.sh
+cp is_complete.sh ../${my_dir}/is_complete.sh
+cp make_input.sh ../${my_dir}/make_input.sh
+cp make_submit.sh ../${my_dir}/make_submit.sh
+cp envelope_script.sh ../${my_dir}/envelope_script.sh
+
+
+done # i_ens
