486ffef5351e10add2b77ecb3be31d59de320d50
Ioannis Trimis <trimisio@login20.chn.perlmutter.nersc.gov>
2025-05-01 12:11:48 -0700

    nersc

---
+++ pure_gauge_ani_generation/nersc_scripts_16128b719156x348992h/params.sh
@@ -0,0 +1,56 @@
+
+#!/bin/bash
+
+cluster=nersc
+
+init_seed=1158
+n_of_lat=1
+n_of_sub=1
+
+nx=16
+ny=16
+nz=16
+nt=128
+
+# MILC convention in the improved action is: beta=10/g^2
+# Here we use plaquette action and so that is not relevant.
+# arxiv 1205.0781 convention is beta=6/g^2
+
+# Now one has to calculate spatial and temporal beta. 
+
+# beta_s=beta/xi_0
+# beta_t=beta*xi_0
+
+beta_s=2.06067 #in the MILC colde this appears first
+beta_t=25.09797 #and this appears second
+
+beta_name="719156"
+xi_0_name="348992"
+
+warms=0
+trajecs=100
+traj_between_meas=1
+steps_per_trajectory=20
+microcanonical_time_step=0.05
+u0=1.0 # THIS IS !=1 FOR 1-LOOP SYMANZIK
+
+stream="h"
+
+ensemble="16128b719156x348992h"
+lat_name="l16128b719156x348992h"
+out_name="out16128b719156x348992h"
+
+
+directory="/global/cfs/projectdirs/m1416/yannis_puregauge/lattices/l16128b719156x348992h"
+out_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/outputs/l16128b719156x348992h"
+path_build="/global/homes/t/trimisio/my_code/pure_gauge_ani_generation/build"
+run_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/runs/rungenl16128b719156x348992h"
+submit_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/submits/subgenl16128b719156x348992h"
+
+executable="su3_hmc_symzk0_a_dbl_crayintel_20250501"
+
+sbatch_time="20:00:00"
+sbatch_nodes="4"
+sbatch_ntasks="256"
+sbatch_jobname="hmcxg4"
+
---
+++ pure_gauge_ani_generation/nersc_scripts_64128b858814x100000h/params.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cluster=nersc
+
+init_seed=1158
+n_of_lat=1
+n_of_sub=1
+
+nx=64
+ny=64
+nz=64
+nt=128
+
+# MILC convention in the improved action is: beta=10/g^2
+# Here we use plaquette action and so that is not relevant.
+# arxiv 1205.0781 convention is beta=6/g^2
+
+# Now one has to calculate spatial and temporal beta. 
+
+# beta_s=beta/xi_0
+# beta_t=beta*xi_0
+
+beta_s=8.58814 #in the MILC colde this appears first
+beta_t=8.58814 #and this appears second
+
+beta_name="858814"
+xi_0_name="100000"
+
+warms=0
+trajecs=100
+traj_between_meas=1
+steps_per_trajectory=20
+microcanonical_time_step=0.05
+u0=1.0 # THIS IS !=1 FOR 1-LOOP SYMANZIK
+
+stream="h"
+
+ensemble="64128b858814x100000h"
+lat_name="l64128b858814x100000h"
+out_name="out64128b858814x100000h"
+
+
+directory="/global/cfs/projectdirs/m1416/yannis_puregauge/lattices/l64128b858814x100000h"
+out_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/outputs/l64128b858814x100000h"
+path_build="/global/homes/t/trimisio/my_code/pure_gauge_ani_generation/build"
+run_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/runs/rungenl64128b858814x100000h"
+submit_dir="/global/cfs/projectdirs/m1416/yannis_puregauge/submits/subgenl64128b858814x100000h"
+
+executable="su3_hmc_symzk0_a_dbl_crayintel_20250501"
+
+sbatch_time="20:00:00"
+sbatch_nodes="4"
+sbatch_ntasks="256"
+sbatch_jobname="hmcxg1"
+
