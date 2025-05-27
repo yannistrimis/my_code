ca8a9cb05d60b7f217bd727a37f630377efa5660
Ioannis Trimis <trimisio@lq.fnal.gov>
2025-05-27 05:31:42 -0500

    fnal

--- imp_rhmc_ani/fnal_scripts_16128f21b6747m014m070xig3500xiq4300a/params.sh
+++ imp_rhmc_ani/fnal_scripts_16128f21b6747m014m070xig3500xiq4300a/params.sh
@@ -5,8 +5,8 @@
 cluster=fnal
 
 init_seed=1158
-n_of_lat=1
-n_of_sub=1
+n_of_lat=1000
+n_of_sub=3
 
 nx=16
 ny=16
@@ -22,7 +22,7 @@ dyn_mass_2=0.07
 rationals_file="rationals.m014m070"
 
 warms=0
-trajecs=100
+trajecs=5
 traj_between_meas=1
 microcanonical_time_step=0.035714
 steps_per_trajectory=28
@@ -38,8 +38,8 @@ path_build="/home/trimisio/all/my_code/imp_rhmc_ani/build"
 run_dir="/project/ahisq/yannis_dyn/runs/rungenl16128f21b6747m014m070xig3500xiq4300a"
 submit_dir="/project/ahisq/yannis_dyn/submits/subgenl16128f21b6747m014m070xig3500xiq4300a"
 
-executable="su3_rhmd_hisq_a_dbl_gcc12openmpi4_20250508"
-# executable="su3_rhmc_hisq_a_dbl_gcc12openmpi4_20250508"
+# executable="su3_rhmd_hisq_a_dbl_gcc12openmpi4_20250508"
+executable="su3_rhmc_hisq_a_dbl_gcc12openmpi4_20250508"
 
 sbatch_time="20:00:00"
 sbatch_nodes="4"
