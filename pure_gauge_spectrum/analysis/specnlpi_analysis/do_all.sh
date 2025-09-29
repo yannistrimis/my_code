a3c0ad748b7ee694b81eb5da1262e143785885db
Ioannis Trimis <trimisio@lq.fnal.gov>
2025-09-29 11:07:04 -0500

    fnal

--- pure_gauge_spectrum/analysis/specnlpi_analysis/do_all.sh
+++ pure_gauge_spectrum/analysis/specnlpi_analysis/do_all.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-nt=64 # DON'T FORGET TO CHANGE !!!
+nt=32 # DON'T FORGET TO CHANGE !!!
 
-ens_name="1664b704115x181411"
-stream="a"
-masses=("0.00725")
+ens_name="1632b681823x100000a"
+masses=("0.01524")
 mas_len=${#masses[@]}
 
-prefix="naivnlpi"
+prefix="hisqnlpi"
 
-xq_arr=("154707")
-#sinks_arr=("PION_5" "PION_i5" "PION_i" "PION_s")
-sinks_arr=("PION_05" "PION_ij" "PION_i0" "PION_0")
+xq_arr=("1000")
+sinks_arr=("PION_5" "PION_i5" "PION_i" "PION_s")
+#sinks_arr=("PION_05" "PION_ij" "PION_i0" "PION_0")
 mom_arr=("p000")
 
 source1="even_and_odd_wall"
-#source2="even_and_odd_wall/FUNNYWALL1"
-source2="even_and_odd_wall/FUNNYWALL2"
+source2="even_and_odd_wall/FUNNYWALL1"
+#source2="even_and_odd_wall/FUNNYWALL2"
 
 src_label="eowfw"
 
@@ -24,7 +23,7 @@ sinkop1="identity"
 sinkop2="identity"
 
 first=101
-last=2300
+last=4500
 
 output_dir="/home/trimisio/all/spec_data"
 data_dir="/home/trimisio/all/spec_data"
@@ -56,8 +55,8 @@ do
 echo "    ${i_file}"
 
 python3 clean_one.py <<EOF
-${ens_name}${stream}
-spec${prefix}${ens_name}xq${xq}
+${ens_name}
+spec${prefix}${ens_name}_xq${xq}
 ${i_file}
 ${mom}
 ${mass1}
@@ -67,7 +66,7 @@ ${source2}
 ${sinkop1}
 ${sinkop2}
 ${sinks}
-cleanspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}
+cleanspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}
 ${output_dir}
 ${data_dir}
 EOF
@@ -75,40 +74,40 @@ EOF
 
 
 python3 aver_one.py <<EOF
-${ens_name}${stream}
-cleanspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}
+${ens_name}
+cleanspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}
 ${i_file}
-averspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}
+averspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}
 ${data_dir}
 EOF
 
-rm ${data_dir}/l${ens_name}${stream}/cleanspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}.${i_file}a
-rm ${data_dir}/l${ens_name}${stream}/cleanspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}.${i_file}b
+rm ${data_dir}/l${ens_name}/cleanspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}.${i_file}a
+rm ${data_dir}/l${ens_name}/cleanspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}.${i_file}b
 
 
 python3 fold_one.py <<EOF
 ${nt}
-${ens_name}${stream}
-averspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}
+${ens_name}
+averspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}
 ${i_file}
-foldspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}
+foldspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}
 ${data_dir}
 EOF
 
-rm ${data_dir}/l${ens_name}${stream}/averspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}.${i_file}
+rm ${data_dir}/l${ens_name}/averspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}.${i_file}
 
 
 done #i_file
 
 python3 format_corrfitter_one.py <<EOF
-${ens_name}${stream}
-${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}
+${ens_name}
+${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}
 ${first}
 ${last}
 ${data_dir}
 EOF
 
-rm ${data_dir}/l${ens_name}${stream}/foldspec${prefix}${mom}${src_label}${ens_name}xq${xq}_m${mass1}m${mass2}${sinks}.*
+rm ${data_dir}/l${ens_name}/foldspec${prefix}${mom}${src_label}${ens_name}_xq${xq}_m${mass1}m${mass2}${sinks}.*
 
 done #xq
 
