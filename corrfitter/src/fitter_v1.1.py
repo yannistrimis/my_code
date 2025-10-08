1b0b941c55edccfeb77b346c283d9a05e19b2372
yannistrimis <trimisio@msu.edu>
2025-10-08 15:18:29 -0400

    cmse

--- corrfitter/src/fitter_v1.1.py
+++ corrfitter/src/fitter_v1.1.py
@@ -27,8 +27,8 @@ def make_prior(N,M):
 
 #    prior['log(ao)'] = gv.log(gv.gvar(['0.35(1000.0)', '0.05(1000.0)']))
 #    prior['log(dEo)'] = gv.log(gv.gvar(['0.18(0.2)', '1.00(1000.0)']))
-    prior['log(ao)'] = gv.log(gv.gvar(M*['0.3035(10000.0)']))
-    prior['log(dEo)'] = gv.log(gv.gvar(M*['0.379417(10000.0)']))
+    prior['log(ao)'] = gv.log(gv.gvar(M*['0.2526(0.0134)']))
+    prior['log(dEo)'] = gv.log(gv.gvar(M*[' 0.394712(0.008)']))
 
     return prior
 
