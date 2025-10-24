dd4240abb684d28d3b3c779684b6ce58ba52aaa6
Ioannis Trimis <trimisio@lq.fnal.gov>
2025-10-24 12:22:14 -0500

    fnal

--- imp_rhmc_ani/fnal_scripts_1632f21b6747m014m070xig1000xiq1000a/params.sh
+++ imp_rhmc_ani/fnal_scripts_1632f21b6747m014m070xig1000xiq1000a/params.sh
@@ -1,12 +1,11 @@
-
 #!/bin/bash
 #DON'T FORGET TO CHANGE EXE FROM HMD TO HMC U DUMBASS
 
 cluster=fnal
 
 init_seed=1158
-n_of_lat=1
-n_of_sub=1
+n_of_lat=1000
+n_of_sub=5
 
 nx=16
 ny=16
@@ -22,29 +21,29 @@ dyn_mass_2=0.07
 rationals_file="rationals.m014m070"
 
 warms=0
-trajecs=100
+trajecs=5
 traj_between_meas=1
-microcanonical_time_step=0.04167
-steps_per_trajectory=24
+microcanonical_time_step=0.05
+steps_per_trajectory=20
 
 ensemble="1632f21b6747m014m070xig1000xiq1000a"
 lat_name="l1632f21b6747m014m070xig1000xiq1000a"
 out_name="out1632f21b6747m014m070xig1000xiq1000a"
 
 
-directory="/lustre1/ahisq/yannis_dyn/lattices/l1632f21b6747m014m070xig1000xiq1000a"
+directory="/lustre2/ahisq/yannis_dyn/lattices/l1632f21b6747m014m070xig1000xiq1000a"
 out_dir="/project/ahisq/yannis_dyn/outputs/l1632f21b6747m014m070xig1000xiq1000a"
 path_build="/home/trimisio/all/my_code/imp_rhmc_ani/build"
 run_dir="/project/ahisq/yannis_dyn/runs/rungenl1632f21b6747m014m070xig1000xiq1000a"
 submit_dir="/project/ahisq/yannis_dyn/submits/subgenl1632f21b6747m014m070xig1000xiq1000a"
 
-executable="su3_rhmd_hisq_a_dbl_gcc12openmpi4_20250508"
-# executable="su3_rhmc_hisq_a_dbl_gcc12openmpi4_20250508"
+#executable="su3_rhmd_hisq_a_dbl_gcc12openmpi4_20250508"
+executable="su3_rhmc_hisq_a_dbl_gcc12openmpi4_20250508"
 
 sbatch_time="20:00:00"
 sbatch_nodes="4"
 sbatch_ntasks="128"
-sbatch_jobname="ahisq10"
+sbatch_jobname="1010m07"
 sbatch_module1="gcc/12"
 sbatch_module2="openmpi/4"
 
