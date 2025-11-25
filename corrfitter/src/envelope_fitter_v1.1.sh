78d8ab7e06dc8244e8b7e6d7d0f3641eb641e17f
yannistrimis <trimisio@msu.edu>
2025-11-25 13:02:58 -0500

    laptop

--- corrfitter/src/envelope_fitter_v1.1.sh
+++ corrfitter/src/envelope_fitter_v1.1.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-ens_name="1648b694635x139939a"
+ens_name="1632b687348x115792a"
 
 src="eowfw"
 prefix="hisqnlpi"
-taste="PION_5"
+taste="PION_s"
 
-xq="14972"
-mass="0.01494"
+xq="1195"
+mass="0.01521"
 
 mom="p000"
 
-fitdir="/home/trimis/spec_data/l${ens_name}" # CMSE
-dir=${fitdir} # CMSE
+# fitdir="/home/trimis/spec_data/l${ens_name}" # CMSE
+# dir=${fitdir} # CMSE
 
-# fitdir="/home/yannis/Physics/LQCD/spec_data/l${ens_name}" # LAPTOP
-# dir=${fitdir} # LAPTOP
+fitdir="/home/yannis/Physics/LQCD/spec_data/l${ens_name}" # LAPTOP
+dir=${fitdir} # LAPTOP
 
 tdatamin=0
-tdatamax=24
+tdatamax=16
 tstep=1
-tp=48
-n_states=2
-m_states=0
-sn="1.0"
+tp=32
+n_states=1
+m_states=1
+sn="-1.0"
 so="-1.0"
 binsize=1
 
 correlated="corr"
-priors="yes_priors"
+priors="no_priors"
 
-tmin_min=5
-tmin_max=24
+tmin_min=0
+tmin_max=8
 tmin_step=1
 
-tmax_min=10
-tmax_max=24
+tmax_min=12
+tmax_max=12
 tmax_step=1
 
 tmin_one=8
