5b03f19771d6fa68205ab6a25bd834a730d8e957
Ioannis Trimis <trimisio@lq.fnal.gov>
2025-12-03 15:20:51 -0600

    fnal

--- pure_gauge_spectrum/analysis/specnlpi_analysis/do_all_fw2_fast.sh
+++ pure_gauge_spectrum/analysis/specnlpi_analysis/do_all_fw2_fast.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-nt=128 # DON'T FORGET TO CHANGE !!!
+nt=32 # DON'T FORGET TO CHANGE !!!
 
-ens_name="16128b719156x348992a"
-masses=("0.01446")
+ens_name="1632b687348x115792a"
+masses=("0.01521")
 mas_len=${#masses[@]}
 
 prefix="hisqnlpi"
 
-xq_arr=("4000")
-#sinks_arr=("PION_s" "PION_i5" "PION_i" "PION_s")
+xq_arr=("1195")
+#sinks_arr=("PION_5" "PION_i5" "PION_i" "PION_s")
 sinks_arr=("PION_05" "PION_ij" "PION_i0" "PION_0")
 mom_arr=("p000")
 
@@ -23,7 +23,7 @@ sinkop1="identity"
 sinkop2="identity"
 
 first=101
-last=1700
+last=4100
 
 # output_dir="/home/trimis/spec_data"
 # data_dir="/home/trimis/spec_data"
