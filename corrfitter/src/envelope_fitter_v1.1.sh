4627d3df8641b84c870215f2a5b39056582edb00
yannistrimis <trimisio@msu.edu>
2025-12-10 13:57:27 -0500

    laptop

--- corrfitter/src/envelope_fitter_v1.1.sh
+++ corrfitter/src/envelope_fitter_v1.1.sh
@@ -1,28 +1,28 @@
 
 #!/bin/bash
 
-ens_name=""
+ens_name="1648b694635x139939a"
 
 src="eowfw"
 prefix="hisqnlpi"
 taste="PION_s"
 
-xq="1980"
-mass="0.0146"
+xq="14972"
+mass="0.01494"
 
 mom="p000"
 
-fitdir="/home/trimis/spec_data/l${ens_name}" # CMSE
-dir=${fitdir} # CMSE
+#fitdir="/home/trimis/spec_data/l${ens_name}" # CMSE
+#dir=${fitdir} # CMSE
 
-#fitdir="/home/yannis/Physics/LQCD/spec_data/l${ens_name}" # LAPTOP
-#dir=${fitdir} # LAPTOP
+fitdir="/home/yannis/Physics/LQCD/spec_data/l${ens_name}" # LAPTOP
+dir=${fitdir} # LAPTOP
 
 tdatamin=0
-tdatamax=32
+tdatamax=24
 tstep=1
-tp=64
-n_states=0
+tp=48
+n_states=2
 m_states=1
 sn="-1.0"
 so="-1.0"
@@ -33,11 +33,11 @@ priors="no_priors"
 opp="yes"
 
 tmin_min=0
-tmin_max=32
+tmin_max=24
 tmin_step=1
 
-tmax_min=32
-tmax_max=32
+tmax_min=24
+tmax_max=24
 tmax_step=1
 
 tmin_one=60
