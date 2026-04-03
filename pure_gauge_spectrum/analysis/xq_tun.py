2baddc9c358f5146f3737683cf8bb50d45d37047
yannistrimis <trimisio@msu.edu>
2026-04-03 15:16:26 -0400

    laptop

--- pure_gauge_spectrum/analysis/xq_tun.py
+++ pure_gauge_spectrum/analysis/xq_tun.py
@@ -1,6 +1,7 @@
 # my_arr contains N_of_mom rows and N_of_x0 + 1 columns.
 # Its first column is p^2.
 
+import sys
 import numpy as np
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
@@ -9,7 +10,7 @@ from matplotlib import pyplot as plt
 print('\n')
 
 def func(x,a,b):
-    return a+b*x
+    return a*x+b
 
 # INPUT
 N_space = 16
@@ -20,13 +21,20 @@ N_of_mom = 3
 x0_arr=np.array([ 1.0, 1.2, 1.4 ])
 
 my_arr = np.array([\
-[0.0,             0.415365, 0.000686,       0.375605, 0.000646,       0.345529, 0.000518],\
-[1.0,             0.550255, 0.001133,       0.497202, 0.000933,       0.455880, 0.000605],\
-[2.0,             0.662206, 0.003553,       0.596866, 0.002494,       0.546493, 0.001877]])
+#[0.0,             0.415365, 0.000686,       0.375605, 0.000646,       0.345529, 0.000518],\
+#[1.0,             0.550255, 0.001133,       0.497202, 0.000933,       0.455880, 0.000605],\
+#[2.0,             0.662206, 0.003553,       0.596866, 0.002494,       0.546493, 0.001877]])
+#[0.0,             0.491793, 0.000542,       0.443484, 0.000504,       0.406975, 0.000446],\
+#[1.0,             0.610712, 0.000871,       0.550173, 0.000728,       0.503775, 0.000621],\
+#[2.0,             0.712492, 0.001909,       0.641248, 0.001411,       0.586203, 0.001098]])
+[0.0,             0.559310, 0.000509,       0.503046, 0.000450,       0.460540, 0.000400],\
+[1.0,             0.666816, 0.000733,       0.599037, 0.000618,       0.547299, 0.000531],\
+[2.0,             0.761363, 0.001425,       0.683428, 0.001073,       0.623451, 0.000847]])
 
-#ENDETH INPUT
 
 N_boot = 10000
+
+#END OF INPUT
 
 xq0_arr = np.zeros(N_boot)
 
@@ -53,24 +61,24 @@ for i_boot in range(N_boot):
 
     l = l + 1
     p_mean, p_cov = curve_fit(func, my_arr_boot[:,0], my_arr_boot[:,2*j+1],sigma=my_arr_boot[:,2*j+2])
-    x_arr[l,0] = 1/np.sqrt(p_mean[1])
-    x_arr[l,1] = 0.5*np.sqrt(p_cov[1,1])*x_arr[l,0]**3
+    x_arr[l,0] = 1/np.sqrt(p_mean[0])
+    x_arr[l,1] = 0.5*np.sqrt(p_cov[0,0])*x_arr[l,0]**3
     x_arr[l,2] = 1/(x_arr[l,1])
 
 #    print ('bare: %.2f  output: %.5f +- %.5f'%(x0_arr[l],x_arr[l,0],x_arr[l,1]) )
 
-#  print('\n')
 
   if N_of_x0 > 2 :
 
-    coeffs = np.polyfit( x0_arr[:], x_arr[:,0], 2, w=x_arr[:,2] )
-    coeffs[2] = coeffs[2] - x_ren
-    solutions = np.roots(coeffs)
+    c_mean, c_cov = curve_fit( func, x0_arr[:], x_arr[:,0], sigma = x_arr[:,1])
+    c_mean[1] = c_mean[1] - x_ren
+    solutions = np.roots(c_mean)
 
     for ii in range( len(solutions) ):
       if solutions[ii] < ( x_ren + 2.0 ) and solutions[ii] > ( x_ren - 2.0 ) :
         predicted_xq0  = np.real(solutions[ii])
         break
+    print(c_mean,c_cov)
 
     xq0_arr[i_boot] = predicted_xq0
 
@@ -79,6 +87,8 @@ for i_boot in range(N_boot):
 #    plt.errorbar(x0_arr[:],x_arr[:,0],yerr=x_arr[:,1],fmt='o')
 #    plt.show()
 #    print('\n')
+
+  sys.exit()
 
 xq0_avg = 0.0
 sqdiff = 0.0
@@ -92,4 +102,4 @@ for i_boot in range(N_boot):
 
 xq0_sdv = np.sqrt( sqdiff / (N_boot-1) )
 
-print( "xq0 +- d(xq0) = %.6f +- %.6f"%(xq0_avg,xq0_sdv) )
+print( "N_boot = %d, xq0 +- d(xq0) = %.6f +- %.6f"%(N_boot, xq0_avg,xq0_sdv) )
