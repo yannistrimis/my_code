37f032f90859766c4b7349bbaab3f6346432b39e
Ioannis Trimis <trimisio@lq.fnal.gov>
2026-05-05 12:14:09 -0500

    fnal

---
+++ imp_rhmc_ani/fnal_scripts_1610f2b5300xig30m024xiq30a/params.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#DON'T FORGET TO CHANGE EXE FROM HMD TO HMC U DUMBASS
+cluster=fnal
+
+init_seed=1158
+n_of_lat=1
+n_of_sub=1
+
+nx=16
+ny=16
+nz=16
+nt=10
+
+beta_s=1.76667
+beta_t=15.90000
+xiq=3.0
+
+dyn_mass=0.024
+rationals_file="rationals.m024"
+
+warms=0
+trajecs=100
+traj_between_meas=1
+microcanonical_time_step=0.045454545
+steps_per_trajectory=22
+
+ensemble="1610f2b5300xig30m024xiq30a"
+lat_name="l1610f2b5300xig30m024xiq30a"
+out_name="out1610f2b5300xig30m024xiq30a"
+
+
+directory="/lustre2/ahisq/yannis_dyn/lattices/l1610f2b5300xig30m024xiq30a"
+out_dir="/project/ahisq/yannis_dyn/outputs/l1610f2b5300xig30m024xiq30a"
+path_build="/home/trimisio/all/my_code/imp_rhmc_ani/build"
+run_dir="/project/ahisq/yannis_dyn/runs/rungenl1610f2b5300xig30m024xiq30a"
+submit_dir="/project/ahisq/yannis_dyn/submits/subgenl1610f2b5300xig30m024xiq30a"
+
+# executable="su3_rhmd_hisq_a_dbl_gcc12openmpi4_20250508"
+# executable="su3_rhmc_hisq_a_dbl_gcc12openmpi4_20250508"
+
+executable="su3_rhmd_naive_plaq_a_dbl_gcc12openmpi4_20250430"
+# executable="su3_rhmc_naive_plaq_a_dbl_gcc12openmpi4_20250430"
+
+sbatch_time="10:00:00"
+sbatch_nodes="2"
+sbatch_ntasks="64"
+sbatch_jobname="pbp6"
+sbatch_module1="gcc/12"
+sbatch_module2="openmpi/4"
+
--- imp_rhmc_ani/master_scripts_f2/make_scripts.sh
+++ imp_rhmc_ani/master_scripts_f2/make_scripts.sh
@@ -8,19 +8,19 @@ cluster="fnal"
 n_of_ens=1
 
 nx=16
-nt=12
+nt=10
 
-beta=5.29
-beta_name="5290"
+beta=5.30
+beta_name="5300"
 
-xig=3.4
-xig_name="34"
+xig=3.0
+xig_name="30"
 
-xiq=3.4
-xiq_name="34"
+xiq=3.0
+xiq_name="30"
 
-dyn_mass=0.02210
-dyn_mass_name="02210"
+dyn_mass=0.024
+dyn_mass_name="024"
 
 
 warms=0
@@ -28,14 +28,14 @@ trajecs=100
 traj_between_meas=1
 microcanonical_time_step_arr=(0.045454545)
 steps_per_trajectory_arr=(22)
-rationals_file="rationals.m02210"
+rationals_file="rationals.m024"
 
 stream_arr=("a")
 
 sbatch_time="20:00:00"
 sbatch_nodes=4
 sbatch_ntasks=128
-sbatch_jobname_arr=("run4")
+sbatch_jobname_arr=("pbp6")
 
 n_of_sub=1
 n_of_lat=1
@@ -111,7 +111,7 @@ then
 
 cat <<EOF >> ../${my_dir}/params.sh
 
-directory="/lustre1/ahisq/yannis_dyn/lattices/${lat_name}"
+directory="/lustre2/ahisq/yannis_dyn/lattices/${lat_name}"
 out_dir="/project/ahisq/yannis_dyn/outputs/${lat_name}"
 path_build="/home/trimisio/all/my_code/imp_rhmc_ani/build"
 run_dir="/project/ahisq/yannis_dyn/runs/rungen${lat_name}"
